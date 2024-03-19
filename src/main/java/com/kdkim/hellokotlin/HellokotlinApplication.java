b25bb981c23eb5eb1861d05091e236aaa31919a6
apffjfjd <115337453+apffjfjd@users.noreply.github.com>
2024-03-20 01:20:02 +0900

    Initial commit

---
+++ src/main/java/com/kdkim/hellokotlin/HellokotlinApplication.java
@@ -0,0 +1,13 @@
+package com.kdkim.hellokotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellokotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellokotlinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdkim/hellokotlin/HellokotlinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdkim.hellokotlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellokotlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
